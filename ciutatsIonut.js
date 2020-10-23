26a9e6c7996fc37162ab00f8ea87387067e7c85f
iatudor <ionut.atudor@gmail.com>
2020-10-23 15:41:37 +0200

    Bug

---
+++ ciutatsIonut.js
@@ -0,0 +1,9 @@
+function PrintCities() {
+    var a = ["Lleida", Barcelona ", "
+        Paris "];
+        a.forEach(function(entry) {
+            console.log(entry);
+        });
+    }
+
+    PrintCities();
